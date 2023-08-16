cb68cf0f8dd3ee3caecd98e95320ed7be5d86b6a
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-16 05:07:11 -0700

    Add files via upload

---
+++ git 12/style.js
@@ -0,0 +1,7 @@
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var now = new Date();
+var theDay = now.getDay();
+var nameOfToday = dayNames[theDay];
+console.log(dayNames[theDay])
+
+alert(dayNames[theDay])
